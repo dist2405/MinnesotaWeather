6bad77d188499d3922e6c197523676e006c3569a
dist2405 <114367217+dist2405@users.noreply.github.com>
2022-11-09 08:46:03 -0600

    Added summary table

--- server.js
+++ server.js
@@ -35,9 +35,10 @@ app.get('/', (req, res) => {
 });
 
 
-// Example GET request handler for data about a specific year
+// Example GET request handler for data about a specific template
 app.get('/favicon.ico', (req, res) => res.status(204));
 let selection_table = "";
+let summary_table = '';
 let selectionquery = "";
 let heading = "";
 let nextbutton = "";
@@ -51,7 +52,10 @@ app.get('/:selected_template/', (req, res) => {
         
    
         if(req.params.selected_template = "weather"){
-             selectionquery = "SELECT name FROM Types ORDER BY name";
+             selectionquery = "SELECT name,SUM(deaths_direct) AS deaths_direct , SUM(deaths_indirect) AS deaths_indirect\
+             ,SUM(damage_property ) AS damage_property,SUM(damage_crops) AS damage_crops, SUM(injuries_direct) AS injuries_direct\
+             , SUM(injuries_indirect) as injuries_indirect FROM Users LEFT JOIN Types ON Users.type = Types.id GROUP BY name;";
+
              heading = "Types of Weather Systems";
              nextbutton = "/weather/hail/"
              prevbutton = "/weather/tornado/"
@@ -66,13 +70,24 @@ app.get('/:selected_template/', (req, res) => {
         };
             db.all(selectionquery,(err, rows)=>{
                 let i;
+                summary_table = '';
                 selection_table = '';
                 for (i=0;i< rows.length;i++){
                     selection_table = selection_table + ' <option value=' + rows[i].name + '>';
                     selection_table = selection_table  + rows[i].name.replace('"','').replace('"','') + '</option>';
+                    summary_table = summary_table + '<tr><td>' + rows[i].name + '</td>';
+                    summary_table = summary_table + '<td>' + rows[i].deaths_direct + '</td>';
+                    summary_table = summary_table + '<td>' + rows[i].deaths_indirect + '</td>';
+                    summary_table = summary_table + '<td>' + rows[i].damage_property + '</td>';
+                    summary_table = summary_table + '<td>' + rows[i].damage_crops + '</td>';
+                    summary_table = summary_table + '<td>' + rows[i].injuries_direct + '</td>';
+                    summary_table = summary_table + '<td>' + rows[i].injuries_indirect + '</td></tr>';
                 }
+               
+              
                 let response = template.replace('%%SelectionOptions%%',selection_table);
                 response = response.replace('%%heading%%',heading);
+                response = response.replace('%%Weather_table%%',summary_table);
                 response = response.replace('%%next%%',nextbutton);
                 response = response.replace('%%previous%%',prevbutton);
                 res.status(200).type('html').send(response); 
@@ -82,13 +97,17 @@ app.get('/:selected_template/', (req, res) => {
 heading = '';
 response = '';
 
+//get request for a specific grouping
 app.get('/:selected_template/:selected_grouping/', (req, res) => {
     fs.readFile(path.join(template_dir,req.params.selected_template +'.html'), 'utf-8', (err, template) => {
-        console.log(req.params.selected_template);
-        console.log(req.params.selected_grouping);
+
     if(req.params.selected_template = "weather"){
-        selectionquery = "SELECT name FROM Types ";
-        heading = req.params.selected_grouping.replace('_',' ');     
+        selectionquery = "SELECT name FROM Types";
+        tablequery= '';
+        heading = req.params.selected_grouping.replace('_',' ');
+        nextbutton = "/weather/";
+        prevbutton = "/weather/";
+          
    };
   
        db.all(selectionquery,(err, rows) =>{
@@ -97,7 +116,9 @@ app.get('/:selected_template/:selected_grouping/', (req, res) => {
            for (i=0;i< rows.length;i++){
                selection_table = selection_table + ' <option value=' + rows[i].name + '>';
                selection_table = selection_table  + rows[i].name.replace('"','').replace('"','') + '</option>';
-           }
+        };
+           
+           
            let response = template.replace('%%SelectionOptions%%',selection_table);
            response = response.replace('%%heading%%',heading);
            response = response.replace('%%next%%',nextbutton);
